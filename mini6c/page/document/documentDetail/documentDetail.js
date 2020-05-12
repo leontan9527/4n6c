25b7f57409bb2f635aabe5d9f95a182937cde595
wuhuawu2583 <258310386@qq.com>
2020-05-12 14:24:15 +0800

    提交公文代码

--- mini6c/page/document/documentDetail/documentDetail.js
+++ mini6c/page/document/documentDetail/documentDetail.js
@@ -1,66 +1,72 @@
-// page/document/documentDetail/documentDetail.js
+const config = require('../../../config')
+const app = getApp()
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
 
+    console.log('options.userId===='+options.userId)
+    var url=config.domain + '/documentController/detailForMobilePage?id='+options.documentId+'&userId='+options.userId+'&organizationId='+options.organizationId
+
+    this.setData({
+      documentId: options.documentId,
+      userId: options.userId,
+      organizationId: options.organizationId,
+      httpDomainAddr:url
+    })
+    //获取最新消息数据
+    //this.getDocumentDetail()
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  getDocumentDetail: function (e) {
 
+    const self = this
+    var sessionId = app.globalData.sessionId
+    
+    if (sessionId) {
+      wx.request({
+        url: config.domain + '/documentCr/documentDetail',
+        data: {
+          id:self.data.documentId,
+          userId:self.data.userId,
+          organizationId:self.data.organizationId
+        },
+        method: 'POST',
+        header: {
+          'content-type': 'application/x-www-form-urlencoded;charset=UTF-8',
+          'Cookie': 'JSESSIONID=' + sessionId
+        },
+        success(result) {
+  
+          if(result.data.success){
+            
+            var redDoc=result.data.data
+            
+            self.setData({
+              redDoc: redDoc,
+            })
+
+            console.log('redDoc=='+self.data.redDoc)
+          }
+        },
+  
+        fail({ errMsg }) {
+          console.log('【plan/list fail】', errMsg)
+        }
+      })
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  getBlurInputValue: function(e) {
+    
+    var value = e.detail.value
+    this.setData({
+      title:value
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
 
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
--- mini6c/page/document/documentList/documentList.js
+++ mini6c/page/document/documentList/documentList.js
@@ -1,60 +1,128 @@
-// page/document/documentList/documentList.js
+const config = require('../../../config')
+const app = getApp()
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    contentlist: [],   
+    pageSize:10,//返回数据的个数 
+    pageNumber:1,
+    searchLoading: false, //"上拉加载"的变量，默认false，隐藏 
+    searchLoadingComplete: false,  //“没有数据”的变量，默认false，隐藏
+    title:''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    //获取最新消息数据
+    this.getMyDocumentPage()
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  getMyDocumentPage: function (e) {
 
+    const self = this
+    var sessionId = app.globalData.sessionId
+    
+    if (sessionId) {
+      wx.request({
+        url: config.domain + '/documentCr/myDocumentPage',
+        data: {
+          title:self.data.title,
+          pageSize: self.data.pageSize,
+          pageNumber:self.data.pageNumber
+        },
+        method: 'POST',
+        header: {
+          'content-type': 'application/x-www-form-urlencoded;charset=UTF-8',
+          'Cookie': 'JSESSIONID=' + sessionId
+        },
+        success(result) {
+  
+          var contentlistTem = self.data.contentlist;
+          if(result.data.success){
+            
+            var contentlist=result.data.items
+            var totalPage=result.data.totalPage
+
+            if (self.data.pageNumber == 1) {
+              contentlistTem = []
+            }
+            
+            self.setData({
+              contentlist: contentlistTem.concat(contentlist),
+              totalPage:totalPage,
+              userId:result.data.userId,
+              organizationId:result.data.organizationId,
+              searchLoading:false,
+            })
+
+            console.log('contentlist=='+self.data.contentlist)
+          }
+        },
+  
+        fail({ errMsg }) {
+          console.log('【plan/list fail】', errMsg)
+        }
+      })
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  getBlurInputValue: function(e) {
+    
+    var value = e.detail.value
+    this.setData({
+      title:value
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  toDocumentDetail: function (e) {
+    
+    var documentId = e.currentTarget.dataset.documentid
+    var userId=e.currentTarget.dataset.userid
+    var organizationId=e.currentTarget.dataset.organizationid
+    wx.navigateTo({ url: '../documentDetail/documentDetail?documentId='+ documentId+'&userId='+userId+'&organizationId='+organizationId })
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+  // 上滑加载更多数据
+  onReachBottom: function(event) {
+    console.log('上滑动onReachBottom')
+    let that = this;  
 
+    if(!that.data.searchLoadingComplete){ 
+      let pageNumber =that.data.pageNumber+1
+      that.setData({  
+        pageNumber: pageNumber,  //每次触发上拉事件，把searchPageNum+1 
+        searchLoading:true, 
+      }); 
+
+      if(pageNumber>=that.data.totalPage){
+        that.setData({  
+          searchLoadingComplete: true,  //每次触发上拉事件，把searchPageNum+1  
+          searchLoading:false,
+        });
+      }
+      that.getMyDocumentPage()//获取最新数据
+    }  
+    
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
+  // 下拉刷新
+  onPullDownRefresh: function(event) {
+    console.log('onPullDownRefresh')
+    let that = this;  
+  
+    that.setData({  
+      pageNumber: 1,  //每次触发上拉事件，把searchPageNum+1  
+      searchLoading: false, //"上拉加载"的变量，默认false，隐藏 
+      searchLoadingComplete: false  //“没有数据”的变量，默认false，隐藏
+    });  
 
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
+    that.getMyDocumentPage()//获取最新数据
   },
 
   /**
