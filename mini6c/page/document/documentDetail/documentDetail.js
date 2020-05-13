7a4ba442400f7bac75f2ae0d1530a50fb152eabd
wuhuawu2583 <258310386@qq.com>
2020-05-13 14:48:15 +0800

    提交查看公文附件代码
    提交查看公文附件代码

--- mini6c/page/document/documentDetail/documentDetail.js
+++ mini6c/page/document/documentDetail/documentDetail.js
@@ -15,10 +15,12 @@ Page({
       documentId: options.documentId,
       userId: options.userId,
       organizationId: options.organizationId,
-      httpDomainDocumentAddr:url
+      httpDomainDocumentAddr:url,
+      httpDownFileAddr:config.domain + '/fileController/download',
+      httpPreviewImageAddr:config.domain,
     })
     //获取最新消息数据
-    //this.getDocumentDetail()
+    this.getDocumentDetail()
   },
 
   getDocumentDetail: function (e) {
@@ -68,5 +70,53 @@ Page({
     })
   },
 
+   /**
+  * 下载文件并预览
+  */
+  downloadFile: function (e) {
+   
+    let type = e.currentTarget.dataset.type
+    let url = e.currentTarget.dataset.url
+
+    let fileName = new Date().valueOf();
+
+    wx.downloadFile({
+      url: url,
+      filePath: wx.env.USER_DATA_PATH + '/' + fileName + type,
+      success: function (res) {
+        //var filePath = res.tempFilePath;
+        var filePath = res.filePath;
+        console.log("filePath====="+filePath);
+        wx.openDocument({
+          filePath: filePath,
+          success: function (res) {
+            console.log('打开文档成功')
+          },
+          fail: function (res) {
+            console.log(res);
+          },
+          complete: function (res) {
+            console.log(res);
+          }
+        })
+      },
+      fail: function (res) {
+        console.log('文件下载失败');
+      },
+      complete: function (res) { },
+    })
+  },
+
+   //预览单个图片
+   previewImage: function (e) {
+    let that = this;
+    let src = e.currentTarget.dataset.src;
+    console.log(src)
+    wx.previewImage({
+      current: src, // 当前显示图片的http链接
+      urls: [src] // =============重点重点=============
+    })
+ 
+  },
 
 })
